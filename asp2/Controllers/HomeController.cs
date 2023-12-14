b702d6e3507417a96991b7daae6fd1518cb0adec
khanhduy2002tpqn <khanhduy2002tpqn@gmail.com>
2023-12-14 21:14:05 +0700

    ditmeweblol

---
+++ asp2/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using asp2.Data;
+using asp2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace asp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ShowRow = true;
+            var _product = _context.Products.Take(12);
+            return View(_product);
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.ShowAbout = true;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            //ViewBag.ShowAbout = true;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ asp2/Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using asp2.Models;
+
+namespace asp2.Models.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+        public PagingInfo PagingInfo { get; set; } = new PagingInfo();
+
+    }
+}
